9a60dbad128e0fb6bae28237af64a755bde6b8a4
Min <biffboffkev@gmail.com>
2022-11-10 00:11:16 -0500

    added configs for channels

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  # identified_by :current_user
+
+  # def connect
+  #   self.current_user = find_verified_user
+  # end
+
+  # private
+
+  # def find_verified_user
+  #   # get the token from the URL params (see frontend code for this connection as well)
+  #   token = request.params[:token]
+  #   user_id = JWT.decode(token, "SECRET_KEY", true, {algorithm:'HS256'})
+  #   if verified_user = User.find_by(id: user_id)
+  #     verified_user
+  #   else
+  #     reject_unauthorized_connection
+  #   end
   end
 end
