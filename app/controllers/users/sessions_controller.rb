c2000b5bcced3e3208822793fba5821df4722ba7
Min <biffboffkev@gmail.com>
2022-11-10 01:46:48 -0500

    need to fix search

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -45,7 +45,7 @@ class Users::SessionsController < Devise::SessionsController
   def respond_with(resource, _opts = {})
     render json: {
       status: {code: 200, message: 'Logged in sucessfully.'},
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      data: UserSerializer.new(resource)
     }, status: :ok
   end
 
